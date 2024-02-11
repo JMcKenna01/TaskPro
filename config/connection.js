0869104519da1507a4f07c361af3bfcff73c437e
venecoderr <147117046+venecoderr@users.noreply.github.com>
2024-02-11 12:56:08 -0500

    bugfix 4

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,21 @@ import 'dotenv/config'
 let sequelize
 
 if (process.env.JAWSDB_URL) {
+  console.log('Using JAWSDB_URL:', process.env.JAWSDB_URL)
   sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
   sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-)
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  )
 }
 
 export { sequelize }
+
 
